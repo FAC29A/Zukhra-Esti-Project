a3fdebf23c3b41505bbb6dee82f9b10732a639bd
Zukhra <zuhra.galieva.2014@gmail.com>
2023-11-03 12:18:11 +0000

    Testing of opening and closing the modal

---
+++ testing/index.test.js
@@ -0,0 +1,12 @@
+console.log("tests")
+test("Opening and closing the modal", () => {
+    const modal = document.getElementById("modal");
+    const openModalBtn = document.getElementById("open-modal-btn");
+    // Click the open button to open the modal
+    openModalBtn.click();
+    equal(modal.style.display, "block", "Modal should be open"); 
+    const closeModalBtn = document.getElementById("close-modal");
+    // Click the open button to close the modal
+    closeModalBtn.click();
+    equal(modal.style.display, "none", "Modal should be closed");
+});
