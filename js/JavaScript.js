1ee53b90b307c2e1b22f89d818d860cec51fb28c
Esti <stsh7573@gmail.com>
2023-11-07 11:23:02 +0000

    add delete

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -6,77 +6,77 @@ const addTaskModalBtn = document.getElementById("add-task-modal");
 const taskNameInput = document.getElementById("task-name");
 const taskDescriptionTextarea = document.getElementById("task-description");
 const prioritySelect = document.getElementById("priority");
-const toDo = document.getElementById("to-do")
+const toDo = document.getElementById("to-do");
 const addTaskInput = document.getElementById("add-task");
 
 // Function to open the modal
 function openModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 // Function to close the modal
 function closeModal() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 // Function to clear input fields
 function clearInputFields() {
-    taskNameInput.value = "";
-    addTaskInput.value = "";
-    taskDescriptionTextarea.value = "";
+  taskNameInput.value = "";
+  addTaskInput.value = "";
+  taskDescriptionTextarea.value = "";
 }
 
 // Function to create a task element
 function createTaskElement(taskName, taskDescription, priority) {
-    // Generate a unique ID for the task element
-    const taskId = "task-" + new Date().getTime();
+  // Generate a unique ID for the task element
+  const taskId = "task-" + new Date().getTime();
 
-    const taskElement = document.createElement("div");
-    taskElement.classList.add("task-content");
-    taskElement.id = taskId; // Set the ID of the task element
-    taskElement.innerHTML = `<h3>${taskName}</h3>`;
+  const taskElement = document.createElement("div");
+  taskElement.classList.add("task-content");
+  taskElement.id = taskId; // Set the ID of the task element
+  taskElement.innerHTML = `<h3>${taskName}</h3>`;
 
-    if (taskDescription.trim() !== "") {
-        taskElement.innerHTML += `<p>Description: ${taskDescription}</p>`;
-    }
+  if (taskDescription.trim() !== "") {
+    taskElement.innerHTML += `<p>Description: ${taskDescription}</p>`;
+  }
 
-    if (priority.trim() !== "") {
-        taskElement.innerHTML += `<p>Priority: ${priority}</p>`;
-    }
+  if (priority.trim() !== "") {
+    taskElement.innerHTML += `<p>Priority: ${priority}</p>`;
+  }
 
-    return taskElement;
+  return taskElement;
 }
 
 // Function to add a task from the modal
 function addTask() {
-    const taskName = taskNameInput.value;
-    const taskDescription = taskDescriptionTextarea.value;
-    const priority = prioritySelect.value;
+  const taskName = taskNameInput.value;
+  const taskDescription = taskDescriptionTextarea.value;
+  const priority = prioritySelect.value;
 
-    if (taskName.trim() !== "") {
-        const taskElement = createTaskElement(taskName, taskDescription, priority);
+  if (taskName.trim() !== "") {
+    const taskElement = createTaskElement(taskName, taskDescription, priority);
 
-        // Add the task to the task list
-        toDo.appendChild(taskElement);
+    // Add the task to the task list
+    toDo.appendChild(taskElement);
 
-        // Make the task draggable
-        taskElement.setAttribute("draggable", "true");
-        taskElement.addEventListener("dragstart", onDragStart);
+    // Make the task draggable
+    taskElement.setAttribute("draggable", "true");
+    taskElement.addEventListener("dragstart", onDragStart);
 
-        // Clear input fields
-        clearInputFields();
+    // Clear input fields
+    clearInputFields();
 
-        // Close the modal
-        closeModal();
-    }
+    // Close the modal
+    closeModal();
+  }
 }
 
 // Event listener to open modal button
 openModalBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    const taskName = addTaskInput.value;
-    taskNameInput.value = taskName;
-    openModal();
+  event.preventDefault();
+  const taskName = addTaskInput.value;
+  taskNameInput.value = taskName;
+  openModal();
 });
 
 // Event listener to close the modal
@@ -85,26 +85,28 @@ closeModalBtn.addEventListener("click", closeModal);
 // Event listener to add a task from the modal
 addTaskModalBtn.addEventListener("click", addTask);
 
-
-//Drag and Drop 
+//Drag and Drop
 
 function onDragStart(event) {
-    const id = event.target.id;
-    event.dataTransfer.setData('text/plain', id);
+  const id = event.target.id;
+  event.dataTransfer.setData("text/plain", id);
 }
 
 function onDragOver(event) {
-    event.preventDefault();
+  event.preventDefault();
 }
-
 
 function onDrop(event) {
-    const id = event.dataTransfer.getData('text');
+  const id = event.dataTransfer.getData("text");
+  const draggableElement = document.getElementById(id);
+  const dropzone = event.target;
 
-    const draggableElement = document.getElementById(id);
-    const dropzone = event.target;
-
+  // Check if the dropzone is the "bin-container"
+  if (dropzone.id === "delete") {
+    // Delete the task
+    draggableElement.remove();
+  } else {
+    // Move the task to the appropriate section
     dropzone.appendChild(draggableElement);
+  }
 }
-
-
